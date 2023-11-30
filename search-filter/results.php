a5f87cbd98ee5aae2d51032ea4461a263de6ea6a
Creastv <piotr@roial.pl>
2023-11-30 17:09:12 +0100

    first

--- search-filter/results.php
+++ search-filter/results.php
@@ -30,11 +30,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 <?php if ( $query->have_posts() ) { ?>
-</div>
+
     <table id="mieszkania-inw" cellspacing="0" class=" table table-hover" style="width:100%">
         <thead>
             <tr>
-
             <th data-priority="10" ></th>
             <th data-priority="1" class="name" >Numer</th>
             <th data-priority="2" >Powierzchnia</th>
